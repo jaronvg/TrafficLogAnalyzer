77c69dbf94ec081a5f4d542da80b6c2c8a1f084f
Jaron <jgrim.wa@gmail.com>
2021-11-14 18:11:21 -0500

    First commit

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, dialog} = require('electron')
 const path = require('path')
-
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+        preload: path.join(__dirname, 'preload.js'),
+        nodeIntegration: true,
+        contextIsolation: false,
     }
   })
-
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
@@ -41,3 +42,4 @@ app.on('window-all-closed', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
